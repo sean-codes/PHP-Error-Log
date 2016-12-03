41cf942829392d5050e06703dc0c927472be9f57
sean_codes <sean.mann@alpinehomeair.com>
2016-12-03 05:22:12 -0600

    Add files via upload

---
+++ php_log/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html style="background-color: #1F1F1F">
+<head>
+    <!-- Styles -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- Header -->
+	<button class='btn' onclick='clearLog()'>Clear Log</button>
+	<button class='btn' onclick='testError()'>Test Error</button>
+	<h2>CS PHP Log</h2>
+    
+    <!-- Error Log -->
+	<ul id="errors"></ul>
+    
+    <!-- Javascripts -->
+    <script src='javascript.js'></script>
+</body>
+</html>
+
